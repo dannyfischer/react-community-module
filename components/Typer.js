c6c6ee3961e4aa190603d74594ed641a5bf29a29
Danny Fischer <git@danny.fm>
2015-11-14 05:03:59 +0100

    📝 Update typed words in header section

--- components/Typer.js
+++ components/Typer.js
@@ -15,15 +15,13 @@ export default function Typer() {
    let typist = malarkey(el, opts);
    typist.delete(initialText.length)
      .type('Design.').pause(pause).delete(7)
-     .type('UX.').pause(pause).delete(3)
-     .type('UI.').pause(pause).delete(3)
      .type('Front-End.').pause(pause).delete(10)
-     .type('React.').pause(pause).delete(6)
-     .type('PHP.').pause(pause).delete(4)
      .type('Back-End.').pause(pause).delete(9)
+     .type('UI.').pause(pause).delete(3)
+     .type('UX.').pause(pause).delete(3)
+     .type('JavaScript.').pause(pause).delete(11)
+     .type('React.').pause(pause).delete(6)
      .type('Performance.').pause(pause).delete(12)
-     .type('Sass.').pause(pause).delete(5)
-     .type('Build Systems.').pause(pause).delete(14)
-     .type('MV*.').pause(pause).delete(4);
+     .type('Frameworks.').pause(pause).delete(11)
 }
 setTimeout(Typer, 3000);
